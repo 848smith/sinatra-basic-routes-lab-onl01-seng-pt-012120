32b9f5e27d3dd81cb7f3eeedb977e9c94e9bdedc
Andrew Smith <848smith@gmail.com>
2020-04-21 01:45:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is __"
+  end
+  
+  get '/hometown' do
+    "My homwtown is __"
+  end
+  
+  get '/favorite_song' do
+    "My favorite song is __"
+  end
 end
